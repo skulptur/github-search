2474ecf851feff955873742f4754a9d137e4db4b
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2021-10-31 16:02:54 -0300

    make layout responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Flex, Box, Heading, Text, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { Flex, Heading, Text, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import { Trending } from './components/Trending'
 import { Saved } from './components/Saved'
 import { About } from 'components/About'
@@ -6,36 +6,36 @@ import { TabContainer } from 'components/General/TabContainer'
 
 export const App = (): JSX.Element => {
   return (
-    <Flex height='100vh' alignItems='center' justifyContent='center' direction='column'>
-      <Heading marginBottom={6}>Popular Repositories</Heading>
+    <Flex height='100vh' paddingLeft={5} paddingRight={5} alignItems='center' direction='column'>
+      <Heading marginTop={50} marginBottom={6}>
+        Popular Repositories
+      </Heading>
       <Text marginBottom={10}>View trending repositories on Github and keep up-to-date with the best projects!</Text>
 
-      <Box width={600}>
-        <Tabs>
-          <TabList>
-            <Tab>Trending</Tab>
-            <Tab>Saved</Tab>
-            <Tab>About</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <TabContainer>
-                <Trending />
-              </TabContainer>
-            </TabPanel>
-            <TabPanel>
-              <TabContainer>
-                <Saved />
-              </TabContainer>
-            </TabPanel>
-            <TabPanel>
-              <TabContainer>
-                <About />
-              </TabContainer>
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </Box>
+      <Tabs maxWidth='700px'>
+        <TabList>
+          <Tab>Trending</Tab>
+          <Tab>Saved</Tab>
+          <Tab>About</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <TabContainer>
+              <Trending />
+            </TabContainer>
+          </TabPanel>
+          <TabPanel>
+            <TabContainer>
+              <Saved />
+            </TabContainer>
+          </TabPanel>
+          <TabPanel>
+            <TabContainer>
+              <About />
+            </TabContainer>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </Flex>
   )
 }
